995f743a94feb5db6d404d7879038935abaa78c4
Yavuz F <yavuzff@outlook.com>
2026-05-22 18:38:55 +0100

    add final block check and model size match

--- python/minisgl/engine/vector_cache.py
+++ python/minisgl/engine/vector_cache.py
@@ -13,15 +13,15 @@ logger = init_logger(__name__)
 DEFAULT_K = 5
 HIT_RATE_PROBABILITY = 0.3 
 # 1.5B model
-#DEFAULT_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260507_154513_Qwen2.5-1.5B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24/db_ivfpq_subsampled_100pct"
+DEFAULT_1_5B_MODEL_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260507_154513_Qwen2.5-1.5B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24/db_ivfpq_subsampled_100pct"
 # 3B model
-DEFAULT_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260516_232926_Qwen2.5-3B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24-28-32/db_ivfpq_subsampled_100pct"
+DEFAULT_3B_MODEL_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260516_232926_Qwen2.5-3B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24-28-32/db_ivfpq_subsampled_100pct"
 # 7B model
-#DEFAULT_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260514_024813_Qwen2.5-7B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24/db_ivfpq_subsampled_100pct"
+DEFAULT_7B_MODEL_DB_PATH = "/home/yff23/data/semantic-layer-skipping/experiments/batch_20260514_024813_Qwen2.5-7B-Instruct_wmt19_train_40000s_128t_strict_strict_match_c4-8-12-16-20-24/db_ivfpq_subsampled_100pct"
 
-DEFAULT_BACKEND = "cache" # ivfpq, cache, ivfpq_centroids, hot_cache
+DEFAULT_BACKEND = "cache" # ivfpq, sim, ivfpq_centroids, hot_cache
 DEFAULT_METADATA = "distribution" # distribution, ivfpq_store
-DEFAULT_COMPRESSION = "dense" # dense, int8, pca
+DEFAULT_COMPRESSION = "normal" # normal, int8, pca
 DEFAULT_NUM_CACHE_VECTORS = 1000
 DEFAULT_N_PROBE = 64
 
@@ -40,7 +40,7 @@ def _fused_compressed_search(queries: torch.Tensor, C_int: torch.Tensor) -> torc
 class VectorCache:
 
     def __init__(self, num_blocks: int, hidden_size: int, device: torch.device, dtype: torch.dtype, k: int = 5, 
-                 backend: str = DEFAULT_BACKEND, db_path: str = DEFAULT_DB_PATH, metadata_backend: str = DEFAULT_METADATA,
+                 backend: str = DEFAULT_BACKEND, db_path: str = None, metadata_backend: str = DEFAULT_METADATA,
                  num_cache_vectors: int = DEFAULT_NUM_CACHE_VECTORS, hot_vector_ids: list[int] = None,
                  compression_option: str = DEFAULT_COMPRESSION):        
         self.num_blocks = num_blocks
@@ -52,6 +52,16 @@ class VectorCache:
         self.metadata_backend = metadata_backend
         self.compression_option = compression_option
         self.num_cache_vectors = num_cache_vectors
+
+        if db_path is None:
+            if hidden_size == 1536:
+                db_path = DEFAULT_1_5B_MODEL_DB_PATH
+            elif hidden_size == 3072:
+                db_path = DEFAULT_3B_MODEL_DB_PATH
+            elif hidden_size == 3584:
+                db_path = DEFAULT_7B_MODEL_DB_PATH
+            else:
+                raise ValueError(f"No default DB path for hidden size {hidden_size}. Please provide a db_path.")
 
         # optional compressed storage
         self.cache_matrices = []
@@ -81,7 +91,7 @@ class VectorCache:
                     # extract the coarse quantiser centroids from the loaded FAISS Index
                     cpu_index = faiss.index_gpu_to_cpu(self.faiss_db.indexes[block_idx]) if "cuda" in str(device) else self.faiss_db.indexes[block_idx]
                     centroids = cpu_index.quantizer.reconstruct_n(0, cpu_index.nlist)
-                    cache_tensor = torch.from_numpy(centroids).to(device=device, dtype=torch.float32)
+                    cache_tensor = torch.as_tensor(centroids, device=device, dtype=torch.float32)
                     cache_tensor = F.normalize(cache_tensor, p=2.0, dim=1).to(dtype)
                     
                     # map the centroid matrix index to a representative ID using the mode of the cluster's skip counts
@@ -115,9 +125,9 @@ class VectorCache:
                         # just take first M vectors if no hot vector list is provided (simulating a hot vector list for benchmarking)
                         hot_vector_ids = list(self.faiss_db.metadata[block_idx].keys())
                     valid_ids = hot_vector_ids[:self.num_cache_vectors]
-                    hot_vectors = np.vstack([cpu_idx.reconstruct(int(vid)) for vid in valid_ids])
+                    hot_vectors = [torch.as_tensor(cpu_idx.reconstruct(int(vid))) for vid in valid_ids]
                     
-                    cache_tensor = torch.from_numpy(hot_vectors).to(device=device, dtype=torch.float32)
+                    cache_tensor = torch.stack(hot_vectors).to(device=device, dtype=torch.float32)
                     cache_tensor = F.normalize(cache_tensor, p=2.0, dim=1).to(dtype)
                     
                     # map the local cache index (0...M) back to the real FAISS vector ID
@@ -136,7 +146,7 @@ class VectorCache:
                     # proxies W8A16 native compression bandwidth savings
                     # tensor is stored as 8-bit integers to halve memory bandwidth reads at inference
                     self.cache_matrices.append(cache_tensor.to(torch.int8))
-                else: # dense
+                else: # normal
                     self.cache_matrices.append(cache_tensor)
                     
             logger.info_rank0(f"Initialised {self.backend} with '{self.compression_option}' optimisation.")
@@ -144,7 +154,7 @@ class VectorCache:
         # CPU component: metadata store per block
         # maps centroid ID -> number of blocks to skip
         self.cpu_metadata = []
-        for _ in range(num_blocks):
+        for _ in range(num_blocks - 1):
             # random metadata for valid skips - early exit and mid-sized skips are most likely
             # we have random metadata for benchmarking purposes
             metadata = {
@@ -156,6 +166,14 @@ class VectorCache:
 
     def search_gpu(self, block_idx: int, hidden_states: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         """Runs strictly on the GPU. Returns (scores, ids)."""
+        batch_size = hidden_states.shape[0]
+        
+        # the final block cannot skip anywhere, return 0s if queried
+        if block_idx >= self.num_blocks - 1:
+            logger.warning_rank0(f"Block {block_idx} queried for skipping decisions, but it's the final block. Returning 0 scores and -1 IDs.")
+            return torch.zeros((batch_size, self.k), dtype=torch.float32, device=self.device), \
+                   torch.zeros((batch_size, self.k), dtype=torch.int64, device=self.device)
+
         if self.backend == "ivfpq":
             # cast to fp32 and ensure contiguous (required for FAISS)
             queries = hidden_states.to(torch.float32).contiguous()
@@ -170,7 +188,6 @@ class VectorCache:
             if actual_k > 0:
                 scores, ids = index.search(queries, actual_k)
             else:
-                batch_size = hidden_states.shape[0]
                 scores = torch.zeros((batch_size, self.k), dtype=torch.float32, device=self.device)
                 ids = torch.zeros((batch_size, self.k), dtype=torch.int64, device=self.device)
             return scores, ids
@@ -196,9 +213,16 @@ class VectorCache:
         """Runs strictly on the CPU. Maps returned IDs to a final routing decision."""
         batch_size = ids_cpu.shape[0]
         
+        # the final block cannot skip anywhere, return 0s if queried
+        if block_idx >= self.num_blocks - 1:
+            logger.warning_rank0(f"Block {block_idx} queried for skipping decisions, but it's the final block. Returning 0 skips.")
+            return [0] * batch_size
+        
         # select the correct metadata dictionary based on backend
         if self.metadata_backend == "ivfpq_store":
             metadata = self.faiss_db.metadata[block_idx]
+            if self.backend == "cache": # cache should use other metadata, but we still safely fallback
+                fallback_keys = list(metadata.keys())
         else:
             metadata = self.cpu_metadata[block_idx]
             
@@ -220,7 +244,7 @@ class VectorCache:
                     elif self.backend == "ivfpq":
                         proposed_skips = metadata[top_id].skip_count
                     else: # cache backend falling back to ivfpq metadata safely
-                        real_id = list(metadata.keys())[top_id % len(metadata)]
+                        real_id = fallback_keys[top_id % len(fallback_keys)]
                         proposed_skips = metadata[real_id].skip_count
                 else:
                     # ensure ID is within bounds of simulated metadata
